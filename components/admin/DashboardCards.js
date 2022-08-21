dae5342d9c01a5e2ac96b081335e7bff28225a85
ArseneMerci <harsenemerci@gmail.com>
2022-08-21 14:16:27 +0000

    fixed error when there is no data on cards

--- components/admin/DashboardCards.js
+++ components/admin/DashboardCards.js
@@ -2,11 +2,12 @@ import styles from '../../styles/dashCards.module.css';
 import Card from './card';
 
 export default function DahboardCards({ Cards }) {
+  console.log(Cards)
   return (
     <div className={styles.projectCards} >
-      <Card data={Cards[0].completed} title="Completed Orders"/>
-      <Card data={Cards[0].inComplete} title="Incomplete Orders"/>
-      <Card data={Cards[0].completed+ Cards[0].inComplete} title="All Orders"/>
+      <Card data={Cards[0].completed || '-'} title="Completed Orders"/>
+      <Card data={Cards[0].inComplete || '-'} title="Incomplete Orders"/>
+      <Card data={Cards[0].completed?Cards[0].completed+ Cards[0].inComplete:'-'} title="All Orders"/>
     </div>
   );
 }
