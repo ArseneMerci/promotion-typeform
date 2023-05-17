86d47f46a8b7355efa01f5ef398be077477e92c3
Toussaint MANZI <toussaintmanzi006@gmail.com>
2023-05-17 16:52:48 +0200

    modifying items positioning on home page

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,8 @@ import Stepper from 'react-stepper-horizontal'
 import { useRouter } from 'next/router'
 import Head from 'next/head';
 import Link from 'next/link';
-import { BiSearch } from "react-icons/bi"
-import { FaShippingFast,FaCertificate,FaFacebookF } from "react-icons/fa"
+import { FaShippingFast,FaCertificate } from "react-icons/fa"
 import { MdOutlineVerified } from "react-icons/md"
-import { RiDoubleQuotesL,RiInstagramFill } from "react-icons/ri"
-import { IoIosArrowUp,IoIosArrowDown} from "react-icons/io"
-import { GiHamburgerMenu } from "react-icons/gi"
 import profile from '../public/assets/aboute4.jpg'
 
 export default function Home() {
@@ -21,34 +17,32 @@ export default function Home() {
         <meta name="description" content="" />
         <link rel="icon" href="/assets/logo.png" />
       </Head>
-        <div className={`container-fluid ${styles.container}`}>
-            <div className={styles.overlay}>
-                <div className={`row ${styles.navbar}`}>
-                    <div className={`col-lg-2 col-md-6 col-sm-2 ${styles.logoContainer}`}>
-                        <Link href="https://mozaikconcepts.com">
-                            <a>
-                            <Image
-                                src="/assets/logoLight.png"
-                                alt="logo"
-                                width={120}
-                                height={120}
-                            />
-                            </a>
-                        </Link>
-                    </div>
-                    <div className={`col-lg-10 col-md-6 col-sm-6 ${styles.menuContainer}`}>
-                        <Link href="https://www.mozaikconcepts.com/Contact"><button >Contact Us</button></Link>
-                        <span></span>
-                    </div>
+        <div className={styles.container}>
+            <div className={styles.navbar}>
+                <div className={styles.logoContainer}>
+                    <Link href="https://mozaikconcepts.com">
+                        <a>
+                        <Image
+                            src="/assets/logoLight.png"
+                            alt="logo"
+                            width={120}
+                            height={100}
+                        />
+                        </a>
+                    </Link>
                 </div>
-                <div className={`row ${styles.headerContainer}`}>
-                    <div className={`col-lg-12 col-sm-12 ${styles.headerText}`}>
-                        <h1><center>The Interior of Your</center>Home Re-Imagined in 3D</h1>
-                    </div>
-                    <div className={`col-lg-12 col-sm-12 ${styles.headerSubText}`}>
-                        <Link href="/order"><button >Get Started</button></Link>
-                        <span></span>
-                    </div>
+                <div className={styles.menuContainer}>
+                    <Link href="https://www.mozaikconcepts.com/Contact"><button >Contact Us</button></Link>
+                    <span></span>
+                </div>
+            </div>
+            <div className={styles.headerContainer}>
+                <div className={styles.headerText}>
+                    <h1>The Interior of Your Home Re-Imagined in 3D</h1>
+                </div>
+                <div className={styles.headerSubText}>
+                    <Link href="/order"><button >Get Started</button></Link>
+                    <span></span>
                 </div>
             </div>
         </div>
@@ -128,60 +122,7 @@ export default function Home() {
                 </div>              
             </div>
         </div>
-        
-        {/* <div className="container-fluid">
-            <div className={` row ${styles.spaceContainer}`}>
-                <div className={` col-lg-12 col-sm-12 ${styles.spaceContent}`}>
-                    <p>
-                    Whether you are designing your house for the first time or renovating it, we can help you find your style
-                    as well as guiding you using our 3D rendering App and follow up on the whole project until it is completed
-                    </p>
-                </div>
-            </div>
-        </div>
-        <div className="container-fluid">
-            <div className={` row ${styles.showStyleContainer}`}>
-                <div className={` col-lg-4  col-md-12 ${styles.styleContent}`}>
-                    <h4>Tell Us What You Want.</h4>
-                    <p>Through different steps, we get to understand your style and how you
-                        want your space to look like. </p>
-                </div>
-                <div className={` col-lg-8  col-md-12 ${styles.styleImage1}`}>
-                </div>
-            </div>
-        </div>
-        <div className="container-fluid ">
-            <div className={` row ${styles.showStyleContainer } ${styles.mar5} `}>
-            <div className={` col-lg-4  col-md-12 ${styles.styleContent}`} >
-                   <h4>Your Space Re-Imagined in 3D.</h4>
-                    <p>
-                        Using renowned softwares, our designers will come up with 3D concepts 
-                        and moodboards that will help you visualize your desired space. They will also
-                        work hand in hand with you to make sure that they deliver a final product you are satisfied with.
-                    </p>
-                </div>
-                <div className={` col-lg-8  col-md-12 ${styles.styleImage2} order-lg-first`} >
-                   
-                </div>
-
-            </div>
-        </div>
-        <div className="container-fluid">
-            <div className={` row ${styles.showStyleContainer} ${styles.mar5}`}>
-                <div className={` col-lg-4 pt-3 col-md-12 ${styles.styleContent}`}>
-                    <h4>Happiness Guarantee.</h4>
-                    <p>
-                        On top of the 3D concepts and moodboards, our team will also provide you with a shopping list 
-                        of every item included in the design to make shopping list of every item included in the design
-                        to make shopping and the whole experience easy for you.
-                    </p>
-                </div>
-                <div className={` col-lg-8  col-md-12 ${styles.styleImage3}`}>
-                </div>
-            </div>
-        </div> */}
     </div>
-    
   );
 }
 
