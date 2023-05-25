848bae3de9c89e93bdd2035927e8eb97c35b0508
Toussaint MANZI <toussaintmanzi006@gmail.com>
2023-05-25 16:53:23 +0200

    changing payment result design

--- pages/result.js
+++ pages/result.js
@@ -1,6 +1,8 @@
 import { useRouter } from "next/router";
 import Button from "@mui/material/Button";
 import { clearAll } from "../utils/persist";
+import {AiFillCheckCircle} from 'react-icons/ai'
+import styles from '../styles/HomePage.module.css'
 const ResultPage = () => {
     const redirect = () => {
         clearAll();
@@ -8,7 +10,29 @@ const ResultPage = () => {
     };
     return (
         <div className="main">
-            <div className="success-container">
+            <div className={styles.resultMainContainer}>
+                <div className={styles.resultContainer}>
+                    <div>
+                        <div style={{display:'flex',flexDirection:'column', justifyContent:'space-between' , alignItems:'center'}}>
+                            < AiFillCheckCircle className={styles.resultIcon}/>
+                            <h1 className={styles.resultTitle}>Payment succesfully!</h1>
+                            <h1 className={styles.resultTitle2}>Transaction id: 085377489</h1>
+                        </div>
+                        <div className={styles.resultHr}></div>
+                        <div>
+                            <div style={{display:'flex' ,justifyContent:'space-between'}}>
+                                <h1 className={styles.resultTitle2}>Amount Paid</h1>
+                                <h1 className={styles.resultTitle2}>RWF 300K</h1>
+                            </div>
+                            <div style={{display:'flex' ,justifyContent:'space-between'}}>
+                                <h1 className={styles.resultTitle2}>Amount Paid</h1>
+                                <h1 className={styles.resultTitle2}>RWF 300K</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* <div className="success-container">
                 <img
                     src="/assets/success.svg"
                     alt="success"
@@ -20,7 +44,7 @@ const ResultPage = () => {
                         Back To Home
                     </Button>
                 </section>
-            </div>
+            </div> */}
         </div>
     );
 };
