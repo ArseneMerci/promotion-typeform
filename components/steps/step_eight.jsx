59d5a3d2be716d4a887fc45cd76ded258049e373
Toussaint MANZI <toussaintmanzi006@gmail.com>
2023-05-26 13:42:25 +0200

    changing from tailwind to pure css

--- components/steps/step_eight.jsx
+++ components/steps/step_eight.jsx
@@ -1,29 +1,29 @@
 import React from 'react'
-
+import styles from '../../styles/information.module.css'
 
 const StepEight = () => {
     return (
-      <div className="h-full flex justify-center items-start mt-6">
-          <div className="w-[30%] h-[500px] border-[1px] border-gray-300 rounded-2xl py-3 px-4">
-              <h1 className="text-xl font-medium mb-12">Personal Information</h1>
-              <form action="" className="flex flex-col">
-                  <label htmlFor="" className="font-medium">
+      <div className={styles.mainContainer}>
+          <div className={styles.container}>
+              <h1 className={styles.title}>Personal Information</h1>
+              <form action="" className={styles.form}>
+                  <label htmlFor="" className={styles.label}>
                       Full names:
                   </label>
-                  <input type="text" className="border-[1px] border-gray-500 h-9 rounded-[5px] mb-3 placeholder:text-sm px-2" placeholder="Enter full names" required/>
-                  <label htmlFor="" className="font-medium">
+                  <input type="text" className={styles.text} placeholder="Enter full names" required/>
+                  <label htmlFor="" className={styles.label}>
                       Email:
                   </label>
-                  <input type="email" className="border-[1px] border-gray-500 h-9 rounded-[5px] mb-3 placeholder:text-sm px-2" placeholder="Enter email" required/>
-                  <label htmlFor="" className="font-medium">
+                  <input type="email" className={styles.text} placeholder="Enter email" required/>
+                  <label htmlFor="" className={styles.label}>
                       Phone number
                   </label>
-                  <input type="text" className="border-[1px] border-gray-500 h-9 rounded-[5px] mb-3 placeholder:text-sm px-2" placeholder="Enter phone number" required/>
-                  <label htmlFor="" className="font-medium">
+                  <input type="text" className={styles.text} placeholder="Enter phone number" required/>
+                  <label htmlFor="" className={styles.label}>
                       City:
                   </label>
-                  <input type="text" className="border-[1px] border-gray-500 h-9 rounded-[5px] mb-3 placeholder:text-sm px-2" placeholder="Enter city" required/>
-                  <button type="submit" className="bg-black text-white font-medium h-11 rounded">Submit</button>
+                  <input type="text" className={styles.text} placeholder="Enter city" required/>
+                  <button type="submit" className={styles.btn}>Submit</button>
               </form>
           </div>
       </div>
