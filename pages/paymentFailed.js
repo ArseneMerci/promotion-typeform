12d18e032b2be3ceddf1aad0047214908d4dc394
Toussaint MANZI <toussaintmanzi006@gmail.com>
2023-06-05 22:46:27 +0200

    modifying page

--- pages/paymentFailed.js
+++ pages/paymentFailed.js
@@ -1,25 +1,37 @@
 import { useRouter } from "next/router";
 import Button from "@mui/material/Button";
+import Image from 'next/image'
+
 import { clearAll } from "../utils/persist";
 import { useEffect } from "react";
+import styles from '../styles/HomePage.module.css'
+
+
 const PaymentFailedPage = () => {
     const redirect = () => {
         window.location.href = "/order";
     };
     return (
         <div className="main">
-            <div className="success-container">
-                <img
-                    src="/assets/success.svg"
-                    alt="success"
-                    className="success-image"
-                />
-                <section>
-                    <span>Ouups! Payment Failed please Try again</span>
-                    <Button variant="text" className="back-to-home" onClick={redirect}>
+            <div className={styles.resultMainContainer}>
+                <div className={styles.resultContainer}>
+                    <div style={{display:'flex',flexDirection:'column', justifyContent:'space-between' , alignItems:'center',margin:'20px 0px'}}>
+                        <div className={styles.photo}>
+                            <div className={styles.resultPhoto1} style={{display:'flex',}}>
+                                <Image
+                                    src="/assets/decline2.svg"
+                                    alt="success"
+                                    layout="fill"
+                                />
+                            </div>
+                        </div>
+                        <h1 className={styles.resultTitle3}>Failed!</h1>
+                        <h1 className={styles.resultTitle2}>Ouups! Payment Failed please Try again</h1>
+                    </div>
+                        <Button variant="text" className="back-to-home" onClick={redirect}>
                         Back To Order
-                    </Button>
-                </section>
+                        </Button>
+                </div>
             </div>
         </div>
     );
