5f9430872f228a846d49fa9599b2ec2834b8307e
Toussaint MANZI <toussaintmanzi006@gmail.com>
2023-08-16 17:52:26 +0200

    adding responsiveness

--- pages/feedback2.js
+++ pages/feedback2.js
@@ -16,12 +16,12 @@ const feedback2 = () => {
             </Link>
         </div>
         <div className='flex justify-center items-center'>
-            <div className="h-[200px] w-[30%] border-[1px] border-amber-500 rounded py-3">
-                <h1 className="text-black font-medium text-center text-2xl">{`We'd love to hear from you`}</h1>
-                <h1 className="text-black text-center">Choose one based on service you experienced</h1>
-                <div className="flex justify-between px-5 mt-5">
-                    <button className="bg-amber-600 text-white cursor-pointer px-4 py-2 rounded font-medium" onClick={()=> dispatch(setConcept())}><Link href='/feedback'>Concept</Link></button>
-                    <button className="bg-amber-600 text-white cursor-pointer px-4 py-2 rounded font-medium" onClick={()=> dispatch(setManagement())}><Link href='/feedback'>Project management</Link></button>
+            <div className=" w-[30%] border-[1px] border-amber-500 rounded py-3 2xl:w-[40%] larg:w-[60%] md:w-[80%] md:mt-32 sm:w-[90%] xtra:w-full">
+                <h1 className="text-black font-medium text-center text-2xl xs:text-xl">{`We'd love to hear from you`}</h1>
+                <h1 className="text-black text-center xs:text-sm">Choose one based on service you experienced</h1>
+                <div className="flex justify-between mx-auto w-[80%] mt-5 xs:text-sm xs:w-[90%]">
+                    <button className="bg-amber-600 text-white cursor-pointer px-4 py-2 rounded font-medium sm:text-sm xs:text-xs" onClick={()=> dispatch(setConcept())}><Link href='/feedback'>Concept</Link></button>
+                    <button className="bg-amber-600 text-white cursor-pointer px-4 py-2 rounded font-medium sm:text-sm xs:text-xs" onClick={()=> dispatch(setManagement())}><Link href='/feedback'>Project management</Link></button>
                 </div>
             </div>
         </div>
