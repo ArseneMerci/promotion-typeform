21703bcf703d8240d9d87f5c41f2f4d0e6b95215
Toussaint MANZI <toussaintmanzi006@gmail.com>
2023-09-29 10:25:56 +0200

    changing button and stepper

--- pages/feedback.js
+++ pages/feedback.js
@@ -13,7 +13,6 @@ import Stepper from 'react-stepper-horizontal';
 import { useDispatch, useSelector } from 'react-redux';
 import { next, prev } from '../state/slices/pageSlice';
 
-
 const feedback = () => {
     const [ fullName,setFullName ] = useState(null);
     const [ phone , setPhone ] = useState(null);
@@ -86,20 +85,21 @@ const feedback = () => {
             </Link>
         </div>
         <div className={`${styles.container} border-[1px] border-amber-500 rounded`}>
-            {type === 'management' && <Stepper steps={ [{title: 'Concept'}, {title: `Project Management`}] } activeStep={ count } className='w-full'/>}
+            {type === 'management' && <Stepper steps={ [{title: ''}, {title: ``}] } activeStep={ count } className='w-full p-0'
+            />}
             {count === 0 && <div>
                 <h1 className={`${styles.maintitle} mt-[20px]`}>{`Concept`}</h1>
 
                 <div>
-                    <h1 className={styles.title}>{`1.Timeliness of clarity of communication regarding project updates and changes`}</h1>
+                    <h1 className={styles.title}>{`1.Timely and clear project updates and changes.`}</h1>
                     <EmojiSlider questionIndex={'Timeliness of clarity of communication regarding project updates and changes'} onEmojiSelect={(emojiIndex) => handleEmojiSelect('Timeliness of clarity of communication regarding project updates and changes', emojiIndex)}/>
                 </div>
                 <div>
-                    <h1 className={styles.title}>{`2.Ability to understand and address your design preferences and requirements`}</h1>
+                    <h1 className={styles.title}>{`2.Understanding and meeting your design needs.`}</h1>
                     <EmojiSlider questionIndex='Ability to understand and address your design preferences and requirements' onEmojiSelect={(emojiIndex) => handleEmojiSelect('Ability to understand and address your design preferences and requirements', emojiIndex)}/>
                 </div>
                 <div>
-                    <h1 className={styles.title}>{`3.Professionalism and courtesy of our staff`}</h1>
+                    <h1 className={styles.title}>{`3.Professionalism and courtesy of our staff.`}</h1>
                     <EmojiSlider questionIndex={'Professionalism and courtesy of our staff'} onEmojiSelect={(emojiIndex) => handleEmojiSelect('Professionalism and courtesy of our staff', emojiIndex)}/>
                 </div>
                 <div>
@@ -107,7 +107,7 @@ const feedback = () => {
                     <EmojiSlider questionIndex={'Responsiveness to your feedback and requests'} onEmojiSelect={(emojiIndex) => handleEmojiSelect('Responsiveness to your feedback and requests', emojiIndex)}/>
                 </div>
                 <div>
-                    <h1 className={styles.title}>{`5.Overall satisfaction with the interior design service provided.`}</h1>
+                    <h1 className={styles.title}>{`5.Satisfaction with interior design service.`}</h1>
                     <EmojiSlider questionIndex={'Overall satisfaction with the interior design service provided'} onEmojiSelect={(emojiIndex) => handleEmojiSelect('Overall satisfaction with the interior design service provided', emojiIndex)}/>
                 </div>
             </div>}
@@ -118,11 +118,11 @@ const feedback = () => {
                     <EmojiSlider questionIndex={'Adherence to project timelines and deadlines'} onEmojiSelect={(emojiIndex) => handleEmojiManagementSelect('Adherence to project timelines and deadlines', emojiIndex)}/>
                 </div>
                 <div>
-                    <h1 className={styles.title}>{`2. Handling of unexpected challenges or changes during the project`}</h1>
+                    <h1 className={styles.title}>{`2. Handling of unexpected challenges or changes during the project.`}</h1>
                     <EmojiSlider questionIndex={'Handling of unexpected challenges or changes during the project'} onEmojiSelect={(emojiIndex) => handleEmojiManagementSelect('Handling of unexpected challenges or changes during the project', emojiIndex)}/>
                 </div>
                 <div>
-                    <h1 className={styles.title}>{`3.Professionalism and courtesy of our staff`}</h1>
+                    <h1 className={styles.title}>{`3.Professionalism and courtesy of our staff.`}</h1>
                     <EmojiSlider questionIndex={'Professionalism and courtesy of our staff'} onEmojiSelect={(emojiIndex) => handleEmojiManagementSelect('Professionalism and courtesy of our staff', emojiIndex)}/>
                 </div>
                 <div>
@@ -139,12 +139,12 @@ const feedback = () => {
                 <input type="text" value={phone} className="px-2" placeholder="Enter email or phone number" onChange={(e)=>setPhone(e.target.value)} required/>
             </form>:''}
             <div className='flex '>
-                {type === 'management' && count === 1 ? <button className='bg-black text-white my-7 rounded w-[150px] h-[40px] mr-5' onClick={()=>dispatch(prev())}>Previous</button> : ''}
-                {count === 1 || type === 'concept' ? <div onClick={handleSubmitFeedback} className={!loader ? 'flex justify-center cursor-pointer items-center bg-black text-white my-7 rounded w-[150px] h-[40px]' : 'flex justify-center items-center text-white my-7 rounded w-[150px] h-[40px] bg-gray-700 cursor-not-allowed'}> 
-                    {!loader ? <h1 className='text-white font-semibold'>Submit</h1>
+                {type === 'management' && count === 1 ? <button className={`${styles.popp} bg-[#233044] text-[#cdcfd0] my-7 rounded w-[150px] h-[40px] mr-5`} onClick={()=>dispatch(prev())}>Previous</button> : ''}
+                {count === 1 || type === 'concept' ? <div onClick={handleSubmitFeedback} className={!loader ? 'flex justify-center cursor-pointer items-center bg-[#233044] text-[#cdcfd0] my-7 rounded w-[150px] h-[40px]' : 'flex justify-center items-center text-white my-7 rounded w-[150px] h-[40px] bg-gray-700 cursor-not-allowed'}> 
+                    {!loader ? <h1 className={`${styles.popp} text-[#cdcfd0]`}>Submit</h1>
                     :
                     <div role="status" className='flex'>
-                        <h1 className='text-white font-semibold mr-3 '>Processing...</h1>
+                        <h1 className={`${styles.popp} text-[#cdcfd0] mr-3`}>Processing...</h1>
                         <svg aria-hidden="true" className="w-5 h-5 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
                             <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
@@ -153,7 +153,7 @@ const feedback = () => {
                     </div>}
                 </div>:''}
             </div>
-            {type === 'management' && count === 0 ? <button className='bg-black text-white my-7 rounded w-[150px] h-[40px]' onClick={()=>dispatch(next())}>Next</button> : ''}
+            {type === 'management' && count === 0 ? <button className='bg-[#233044] text-[#cdcfd0] my-7 rounded w-[150px] h-[40px]' onClick={()=>dispatch(next())}>Next</button> : ''}
         </div>
     </div>
   )
